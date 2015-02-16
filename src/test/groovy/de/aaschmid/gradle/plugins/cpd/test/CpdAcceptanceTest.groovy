649e82a705c1942647b09640c11fe17c1986ea99
Andreas Schmid <service@aaschmid.de>
2015-02-16 22:12:37 +0100

    fixed tests using new PMD version

--- src/test/groovy/de/aaschmid/gradle/plugins/cpd/test/CpdAcceptanceTest.groovy
+++ src/test/groovy/de/aaschmid/gradle/plugins/cpd/test/CpdAcceptanceTest.groovy
@@ -156,7 +156,7 @@ class CpdAcceptanceTest extends BaseSpec {
         given:
         project.cpd{
             encoding = 'ISO-8859-1'
-            minimumTokenCount = 5
+            minimumTokenCount = 10
         }
         project.cpdCheck.source = testFile('de/aaschmid/foo')
 
@@ -191,7 +191,7 @@ class CpdAcceptanceTest extends BaseSpec {
 
         def report = project.file('build/reports/cpd/cpdCheck.csv')
         report.exists()
-        report.text =~ /7,19,2,5,.*Clazz1.java,5,.*Clazz2.java/
+        report.text =~ /7,20,2,5,.*Clazz1\.java,5,.*Clazz2\.java/
     }
 
     def "executing 'Cpd' task on duplicate 'java' source should not throw 'GradleException' if 'ignoreFailures' and produce 'cpdCheck.csv' with one warning"() {
@@ -214,7 +214,7 @@ class CpdAcceptanceTest extends BaseSpec {
 
         def report = project.file('build/reports/cpd/cpdCheck.csv')
         report.exists()
-        report.text =~ /7,19,2,5,.*Clazz1.java,5,.*Clazz2.java/
+        report.text =~ /7,20,2,5,.*Clazz1\.java,5,.*Clazz2\.java/
     }
 
     def "applying 'Cpd' task to only parent project if only sub project has 'groovy' plugin"() {
