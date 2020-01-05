8b4a78803bbf97c325bba1ac9d2fa0cb8199e120
Andreas Schmid <service@aaschmid.de>
2020-01-05 18:35:46 +0100

    migrate version, description and extra properties to kotlin-dsl
    Signed-off-by: Andreas Schmid <service@aaschmid.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,10 @@ plugins {
     id("com.jfrog.bintray") version "1.8.5"
 }
 
+description = "Gradle plugin to find duplicate code using PMDs copy/paste detection (= CPD)"
+group = "de.aaschmid"
+version = "3.3-SNAPSHOT"
+
+val isBuildOnJenkins by extra(System.getenv("BUILD_TAG")?.startsWith("jenkins-") ?: false)
+
 apply(from = "legacy-build.gradle")
